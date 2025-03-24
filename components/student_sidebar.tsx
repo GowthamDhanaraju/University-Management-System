35af021d3813e9d8dc71454d14e298ade0e8dd42
vyshnav-182005 <vyshnavkumar1825@gmail.com>
2025-03-24 21:18:17 +0530

    student sidebar

--- components/student_sidebar.tsx
+++ components/student_sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { FiBook, FiUser, FiBarChart2, FiCheckSquare, FiTruck, FiClipboard, FiAward } from "react-icons/fi";
+import { FiBook, FiUser, FiBarChart2, FiTruck, FiClipboard, FiAward } from "react-icons/fi";
 
 
 const StudentSidebar: React.FC = () => {
